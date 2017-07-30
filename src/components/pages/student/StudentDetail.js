72e9b4f6460f1a7b95e1afffa9dded23b704b12a
myyusuf <myyusuf1911@gmail.com>
2017-07-30 13:47:36 +0700

    Student details fix layout

--- src/components/pages/student/StudentDetail.js
+++ src/components/pages/student/StudentDetail.js
@@ -3,75 +3,111 @@ import PropTypes from 'prop-types';
 import Tag from 'antd/lib/tag';
 import Button from 'antd/lib/button';
 import Icon from 'antd/lib/icon';
+import Row from 'antd/lib/row';
+import Col from 'antd/lib/col';
 import Progress from 'antd/lib/progress';
 import { PieChart, Pie, Sector, Cell } from 'recharts';
 import './StudentDetail.css';
 
 const data = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
                   {name: 'Group C', value: 300}, {name: 'Group D', value: 200}];
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const COLORS = ['#5093E1', '#50C14E', '#F65177', '#9DA5BE'];
 
 const RADIAN = Math.PI / 180;
 
 const StudentDetail = ({ student }) => (
   <div className="StudentDetail-container">
-    <div>
-      <span className="StudentDetail-name">{ student.name }</span>
-      <span style={{ marginLeft: 20 }}><Tag>Level 1</Tag></span>
-    </div>
-    <div style={{ fontWeight: 'bold', fontSize: 15 }}>
-      <span>{ student.newSid } { student.oldSid }</span>
-    </div>
-    <div className="StudentDetail-contact">
-      <span>{ student.email }</span>
-      <span>{ student.phone }</span>
-    </div>
-    <div>
-      <Button type="primary" style={{ marginTop: 20, backgroundColor: '#50C14E', borderColor: '#50C14E' }}>
-        Send SMS
-        <Icon type="message" style={{ fontSize: 14 }} />
-      </Button>
-      <Button type="primary" style={{ marginTop: 20, marginLeft: 10, backgroundColor: '#50C14E', borderColor: '#50C14E' }}>
-        Call
-        <Icon type="mobile" style={{ fontSize: 14 }} />
-      </Button>
-    </div>
-    <div>
-      <PieChart width={400} height={220} onMouseEnter={this.onPieEnter}>
-        <Pie
-          data={data}
-          cx={120}
-          cy={120}
-          innerRadius={60}
-          outerRadius={80}
-          fill="#8884d8"
-          paddingAngle={0}
-        >
-        	{
-          	data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
-          }
-        </Pie>
-      </PieChart>
-    </div>
-    <div style={{ marginTop: 30 }}>
-      <span style={{ fontSize: 11, fontWeight: 'bold', color: 'gray' }}>OTHER RESOURCES</span>
-    </div>
-    <div style={{ marginTop: 10 }}>
-      <Button type="">
-        UKMPPD Score
-      </Button>
-      <Button type="" style={{ marginLeft: 10 }}>
-        Problems
-      </Button>
-    </div>
-    <div style={{ marginTop: 20 }}>
-      <span style={{ fontSize: 11, fontWeight: 'bold', color: 'gray' }}>ACTION</span>
-    </div>
-    <div style={{ marginTop: 10 }}>
-      <Button type="danger">
-        Delete Student
-      </Button>
-    </div>
+    <Row>
+      <Col span={24}>
+        <span className="StudentDetail-name">{ student.name }</span>
+        <span style={{ marginLeft: 20 }}><Tag>Level 1</Tag></span>
+      </Col>
+    </Row>
+    <Row>
+      <Col span={24}>
+        <div style={{ fontWeight: 'bold', fontSize: 15 }}>
+          <span>{ student.newSid } { student.oldSid }</span>
+        </div>
+      </Col>
+    </Row>
+    <Row>
+      <Col span={24}>
+        <div className="StudentDetail-contact">
+          <span>{ student.email }</span>
+          <span>{ student.phone }</span>
+        </div>
+      </Col>
+    </Row>
+    <Row>
+      <Col span={24}>
+        <Button type="primary" style={{ marginTop: 20, backgroundColor: '#50C14E', borderColor: '#50C14E' }}>
+          Send SMS
+          <Icon type="message" style={{ fontSize: 14 }} />
+        </Button>
+        <Button type="primary" style={{ marginTop: 20, marginLeft: 10, backgroundColor: '#50C14E', borderColor: '#50C14E' }}>
+          Call
+          <Icon type="mobile" style={{ fontSize: 14 }} />
+        </Button>
+      </Col>
+    </Row>
+    <Row>
+      <Col span={12}>
+        <div style={{ position: 'relative', top: 92, left: 56, textAlign: 'center', width: 100 }}>
+          <span style={{ fontSize: 30, fontWeight: 'bold', color: 'gray' }}>7</span><br />
+          <span style={{ fontWeight: 'bold' }}>Courses</span>
+        </div>
+        <PieChart style={{ top: -20 }} width={200} height={170} onMouseEnter={this.onPieEnter}>
+          <Pie
+            data={data}
+            cx={100}
+            cy={80}
+            innerRadius={60}
+            outerRadius={80}
+            fill="#8884d8"
+            paddingAngle={0}
+            label={false}
+          >
+          	{
+            	data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+            }
+          </Pie>
+        </PieChart>
+      </Col>
+      <Col span={12}>
+        <div style={{ marginTop: 80 }}><Tag color="#5093E1">3</Tag> Completed </div>
+        <div style={{ marginTop: 5 }}><Tag color="#50C14E">2</Tag> Problems </div>
+        <div style={{ marginTop: 5 }}><Tag color="#F65177">1</Tag> Pendings </div>
+      </Col>
+    </Row>
+    <Row>
+      <Col span={24}>
+        <div className="StudentDetail-contact">
+          <span style={{ fontSize: 11, fontWeight: 'bold', color: 'gray' }}>OTHER RESOURCES</span>
+        </div>
+      </Col>
+    </Row>
+    <Row style={{ marginTop: 10 }}>
+      <Col span={24}>
+        <Button type="">
+          UKMPPD Score
+        </Button>
+        <Button type="" style={{ marginLeft: 10 }}>
+          Problems
+        </Button>
+      </Col>
+    </Row>
+    <Row style={{ marginTop: 20 }}>
+      <Col span={24}>
+        <span style={{ fontSize: 11, fontWeight: 'bold', color: 'gray' }}>ACTION</span>
+      </Col>
+    </Row>
+    <Row style={{ marginTop: 10 }}>
+      <Col span={24}>
+        <Button type="danger">
+          Delete Student
+        </Button>
+      </Col>
+    </Row>
   </div>
 );
 
