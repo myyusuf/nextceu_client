d84ad34fcd7635e765c527362fdf4f94cfb5d9dd
myyusuf <myyusuf1911@gmail.com>
2017-08-24 14:22:29 +0700

    Hospital form

---
+++ src/components/hospital/HospitalForm.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Form from 'antd/lib/form';
+import Row from 'antd/lib/row';
+import Col from 'antd/lib/col';
+import Input from 'antd/lib/input';
+import Button from 'antd/lib/button';
+
+const FormItem = Form.Item;
+
+const HospitalForm = ({ code, name, hospitalFormChanged }) => (
+  <Form>
+    <Row>
+      <Col span={24}>
+        <FormItem
+          label="Code"
+          colon={false}
+          validateStatus={code.validateStatus}
+          help={code.errorMsg}
+        >
+          <Input
+            value={code.value}
+            onChange={(e) => {
+              hospitalFormChanged({
+                name: 'code',
+                value: e.target.value,
+              });
+            }}
+            placeholder="Code"
+          />
+        </FormItem>
+      </Col>
+    </Row>
+    <Row>
+      <Col span={24}>
+        <FormItem
+          label="Name"
+          colon={false}
+          validateStatus={name.validateStatus}
+          help={name.errorMsg}
+        >
+          <Input
+            value={name.value}
+            onChange={(e) => {
+              hospitalFormChanged({
+                name: 'name',
+                value: e.target.value,
+              });
+            }}
+            placeholder="Name"
+          />
+        </FormItem>
+      </Col>
+    </Row>
+    <Row>
+      <Col span={24}>
+        <Button />
+      </Col>
+    </Row>
+  </Form>
+);
+
+HospitalForm.propTypes = {
+  hospitalFormChanged: PropTypes.func.isRequired,
+  code: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default HospitalForm;
