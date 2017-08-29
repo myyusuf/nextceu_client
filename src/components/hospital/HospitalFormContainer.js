23c66a362a83a3d000ef9e5d58fac3c3501235e5
myyusuf <myyusuf1911@gmail.com>
2017-08-29 21:12:24 +0700

    Hospital delete

--- src/components/hospital/HospitalFormContainer.js
+++ src/components/hospital/HospitalFormContainer.js
@@ -4,11 +4,17 @@ import PropTypes from 'prop-types';
 import Row from 'antd/lib/row';
 import Col from 'antd/lib/col';
 import Button from 'antd/lib/button';
+import Modal from 'antd/lib/modal';
 import HospitalFormWrapper from '../../containers/hospital/HospitalFormWrapper';
+
+const confirm = Modal.confirm;
 
 const HospitalFormContainer = ({
   onOk,
   confirmLoading,
+  hospitalId,
+  hospitalName,
+  confirmDelete,
 }) => (
   <div style={{ paddingLeft: 20, paddingRight: 20 }}>
     <Row>
@@ -18,8 +24,17 @@ const HospitalFormContainer = ({
     </Row>
     <Row>
       <Col>
-        <Button type="primary" loading={confirmLoading} onClick={onOk}>
+        <Button
+          type="primary"
+          loading={confirmLoading}
+          onClick={onOk}
+          disabled={hospitalId === undefined}
+          style={{ marginRight: 10 }}
+        >
           Save
+        </Button>
+        <Button type="danger" loading={confirmLoading} onClick={() => confirmDelete(hospitalId, hospitalName)} disabled={hospitalId === undefined}>
+          Delete
         </Button>
       </Col>
     </Row>
@@ -29,11 +44,16 @@ const HospitalFormContainer = ({
 HospitalFormContainer.propTypes = {
   onOk: PropTypes.func.isRequired,
   confirmLoading: PropTypes.bool.isRequired,
+  hospitalId: PropTypes.number.isRequired,
+  hospitalName: PropTypes.string.isRequired,
+  confirmDelete: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => (
   {
     confirmLoading: state.hospitalReducers.hospitalFormContainer.confirmLoading,
+    hospitalId: state.hospitalReducers.hospitalForm.id.value,
+    hospitalName: state.hospitalReducers.hospitalForm.name.value,
   }
 );
 
@@ -44,6 +64,21 @@ const mapDispatchToProps = dispatch => (
         type: 'SAVE_HOSPITAL_LOGIC',
       });
     },
+    confirmDelete: (hospitalId, hospitalName) => (
+      confirm({
+        title: `Do you Want to delete hospital: ${hospitalName}`,
+        content: 'This action cannot be undone',
+        onOk() {
+          dispatch({
+            type: 'DELETE_HOSPITAL_LOGIC',
+            payload: { id: hospitalId },
+          });
+        },
+        onCancel() {
+          console.log('Cancel');
+        },
+      })
+    ),
   }
 );
 
