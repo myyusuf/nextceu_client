a6f2001745e279661b98b321a355cef4d3c5c268
myyusuf <myyusuf1911@gmail.com>
2017-09-03 19:10:03 +0700

    Fix styling

--- src/components/hospital/HospitalListItem.js
+++ src/components/hospital/HospitalListItem.js
@@ -6,32 +6,52 @@ import Col from 'antd/lib/col';
 import Badge from 'antd/lib/badge';
 import './HospitalListItem.css';
 
-const HospitalListItem = ({ hospital, selectHospital }) => (
-  <Card
-    onClick={() => selectHospital(hospital)}
-    className={hospital.selected ? 'HospitalList-item-selected' : 'HospitalList-item'}
-  >
-    <Row gutter={10}>
-      <Col span={3}>
-        <Badge
-          count={hospital.studentsInDepartmentCount}
-          overflowCount={1000}
-          style={{ backgroundColor: '#87d068' }}
-        />
-      </Col>
-      <Col span={3}>
-        <Badge
-          count={hospital.departmentQuota}
-          overflowCount={1000}
-          style={{ backgroundColor: '#fff', color: '#999', boxShadow: '0 0 0 1px #d9d9d9 inset' }}
-        />
-      </Col>
-      <Col span={18}>
-        <div style={{ marginLeft: 5, fontSize: 13 }}>{hospital.name}</div>
-      </Col>
-    </Row>
-  </Card>
-);
+const HospitalListItem = ({ hospital, selectHospital }) => {
+  const departmentQuota = parseInt(hospital.departmentQuota, 10);
+  const studentsInDepartmentCount = parseInt(hospital.studentsInDepartmentCount, 10);
+  let badge = (
+    <Badge
+      count={hospital.studentsInDepartmentCount}
+      overflowCount={1000}
+      style={{ backgroundColor: '#87d068' }}
+      showZero
+    />
+  );
+  if (studentsInDepartmentCount > departmentQuota) {
+    badge = (
+      <Badge
+        count={hospital.studentsInDepartmentCount}
+        overflowCount={1000}
+        showZero
+      />
+    );
+  }
+
+  return (
+    <Card
+      onClick={() => selectHospital(hospital)}
+      className={hospital.selected ? 'HospitalList-item-selected' : 'HospitalList-item'}
+    >
+      <Row gutter={10}>
+        <Col span={3}>
+          {badge}
+        </Col>
+        <Col span={3}>
+          <Badge
+            count={hospital.departmentQuota}
+            overflowCount={1000}
+            style={{ backgroundColor: '#fff', color: '#999', boxShadow: '0 0 0 1px #d9d9d9 inset' }}
+            showZero
+          />
+        </Col>
+        <Col span={18}>
+          <div style={{ marginLeft: 5, fontSize: 13 }}>{hospital.name}</div>
+        </Col>
+      </Row>
+    </Card>
+  );
+};
+
 
 HospitalListItem.propTypes = {
   hospital: PropTypes.shape.isRequired,
