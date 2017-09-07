e199c40998d44b0b44bac574873c9520c3f1dc81
myyusuf <myyusuf1911@gmail.com>
2017-09-07 16:55:43 +0700

    Course seminars

--- src/reducers/student/course/course_seminars.js
+++ src/reducers/student/course/course_seminars.js
@@ -8,10 +8,10 @@ const seminars = (state = defaultState, action) => {
     case 'FETCH_COURSE_SEMINARS_SUCCESS':
       return { ...state, seminars: action.payload };
     case 'COURSE_SEMINAR_LOADING_START': {
-      return { ...state, loading: true };
+      return { ...state, loading: true, seminars: [...state.seminars] };
     }
     case 'COURSE_SEMINAR_LOADING_FINISH': {
-      return { ...state, loading: false };
+      return { ...state, loading: false, seminars: [...state.seminars] };
     }
     default:
       return state;
